28e511f3510f0cbb9b9d77e29c45fed202d43f58
Mike Conner <connerland@yahoo.com>
2019-01-27 14:03:21 -0500

    Create Project

---
+++ Project-6/Project-6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-6
+//
+//  Created by Mike Conner on 1/27/19.
+//  Copyright © 2019 Mike Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
