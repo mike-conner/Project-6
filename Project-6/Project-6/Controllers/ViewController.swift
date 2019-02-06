50ba7f6597cb2ab1e0b77cbbfdd610ee7ebd8509
Mike Conner <connerland@yahoo.com>
2019-02-06 07:22:29 -0500

    Retrieve all data - working

--- Project-6/Project-6/Controllers/ViewController.swift
+++ Project-6/Project-6/Controllers/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     let client = SWAPIClient(configuration: .default)
     
     var entity: EntityType?
-    var page = 1                                            // Need to create page variable for each entity and update getListOfEntities() accordingly!!!!
+    var peoplePageCounter = 1
+    var vehiclePageCounter = 1
+    var starshipPageCounter = 1
+    var planetPageCounter = 1
     
     var peopleCollectionList: People?
     var vehicleCollectionList: Vehicles?
@@ -23,10 +26,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getListOfEntities(entity: .people, page: page)
-        getListOfEntities(entity: .vehicles, page: page)
-        getListOfEntities(entity: .starships, page: page)
-        getListOfEntities(entity: .planets, page: page)
+        getListOfEntities(entity: .people, page: peoplePageCounter)
+        getListOfEntities(entity: .vehicles, page: vehiclePageCounter)
+        getListOfEntities(entity: .starships, page: starshipPageCounter)
+        getListOfEntities(entity: .planets, page: planetPageCounter)
     }
 
     
@@ -55,55 +58,53 @@ class ViewController: UIViewController {
     }
     
     func getListOfEntities(entity: EntityType, page: Int) {
-        DispatchQueue.main.async {
-            self.client.getEntityList(entityType: entity, page: self.page) { people, vehicles, starships, planets, error in
-                if let people = people {
-                    if self.peopleCollectionList?.results == nil {
-                        self.peopleCollectionList = people
-                        self.page = self.page + 1
-                    } else {
-                        self.peopleCollectionList?.results.append(contentsOf: people.results)
-                        self.page = self.page + 1
-                    }
-                    if people.next != nil {
-                        self.getListOfEntities(entity: .people, page: self.page)
-                    } 
+        self.client.getEntityList(entityType: entity, page: page) { people, vehicles, starships, planets, error in
+            if let people = people {
+                if self.peopleCollectionList?.results == nil {
+                    self.peopleCollectionList = people
+                    self.peoplePageCounter = page + 1
+                } else {
+                    self.peopleCollectionList?.results.append(contentsOf: people.results)
+                    self.peoplePageCounter = page + 1
                 }
-                if let vehicles = vehicles {
-                    if self.vehicleCollectionList?.results == nil {
-                        self.vehicleCollectionList = vehicles
-                        self.page = self.page + 1
-                    } else {
-                        self.vehicleCollectionList?.results.append(contentsOf: vehicles.results)
-                        self.page = self.page + 1
-                    }
-                    if vehicles.next != nil {
-                        self.getListOfEntities(entity: .vehicles, page: self.page)
-                    }
+                if people.next != nil {
+                    self.getListOfEntities(entity: .people, page: self.peoplePageCounter)
                 }
-                if let starships = starships {
-                    if self.starsthipCollectionList?.results == nil {
-                        self.starsthipCollectionList = starships
-                        self.page = self.page + 1
-                    } else {
-                        self.starsthipCollectionList?.results.append(contentsOf: starships.results)
-                        self.page = self.page + 1
-                    }
-                    if starships.next != nil {
-                        self.getListOfEntities(entity: .starships, page: self.page)
-                    }
+            }
+            if let vehicles = vehicles {
+                if self.vehicleCollectionList?.results == nil {
+                    self.vehicleCollectionList = vehicles
+                    self.vehiclePageCounter = page + 1
+                } else {
+                    self.vehicleCollectionList?.results.append(contentsOf: vehicles.results)
+                    self.vehiclePageCounter = page + 1
                 }
-                if let planets = planets {
-                    if self.planetCollectionList?.results == nil {
-                        self.planetCollectionList = planets
-                        self.page = self.page + 1
-                    } else {
-                        self.planetCollectionList?.results.append(contentsOf: planets.results)
-                        self.page = self.page + 1
-                    }
-                    if planets.next != nil {
-                        self.getListOfEntities(entity: .planets, page: self.page)
-                    }
+                if vehicles.next != nil {
+                    self.getListOfEntities(entity: .vehicles, page: self.vehiclePageCounter)
+                }
+            }
+            if let starships = starships {
+                if self.starsthipCollectionList?.results == nil {
+                    self.starsthipCollectionList = starships
+                    self.starshipPageCounter = page + 1
+                } else {
+                    self.starsthipCollectionList?.results.append(contentsOf: starships.results)
+                    self.starshipPageCounter = page + 1
+                }
+                if starships.next != nil {
+                    self.getListOfEntities(entity: .starships, page: self.starshipPageCounter)
+                }
+            }
+            if let planets = planets {
+                if self.planetCollectionList?.results == nil {
+                    self.planetCollectionList = planets
+                    self.planetPageCounter = page + 1
+                } else {
+                    self.planetCollectionList?.results.append(contentsOf: planets.results)
+                    self.planetPageCounter = page + 1
+                }
+                if planets.next != nil {
+                    self.getListOfEntities(entity: .planets, page: self.planetPageCounter)
                 }
             }
         }
