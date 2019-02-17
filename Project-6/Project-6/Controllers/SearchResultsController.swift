544bce5665d097457424079faff1be6b27345800
Mike Conner <connerland@yahoo.com>
2019-02-16 20:01:23 -0500

    Done

--- Project-6/Project-6/Controllers/SearchResultsController.swift
+++ Project-6/Project-6/Controllers/SearchResultsController.swift
@@ -48,7 +48,6 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(SearchResultsController.reloadEntityData))
         
         title = ""
         activityIndicator.isHidden = false
@@ -73,30 +72,19 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
         }
     }
     
-    @objc func reloadEntityData() {
-        activityIndicator.isHidden = false
-        showActivityIndicator(on: true)
-        entityPageCounter = 1
-        planetPageCounter = 1
-        if let entity = entity {
-            switch entity {
-        case .people:
-            getListOfPeople(page: entityPageCounter)
-            getListOfPlanets(page: planetPageCounter)
-        case .vehicles:
-            getListOfVehicles(page: entityPageCounter)
-        case .starships:
-            getListOfStarships(page: entityPageCounter)
-        default:
-            return
-        }
-        }
-    }
-    
     @objc func dismissSearchResultsController() {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func showActivityIndicator(on: Bool) {
+        if on {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    // MARK: - IBActions to change display of specific data
     @IBAction func changeMonetaryType(_ sender: Any) {
         if resultsTwo.text == "Unknown" {
             let alert = UIAlertController(title: "Cost Unknown", message: "We have no value to convert", preferredStyle: .alert)
@@ -160,6 +148,7 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
         }
     }
     
+    // MARK: - Set up UI with labels and data
     func setUpLabelsBasedOnEntity(entity: EntityType) {
         switch entity {
         case .people:
@@ -186,98 +175,104 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
         }
     }
     
-    func setUpResultsBasedOnEntity(entity: EntityType, index: Int) {
+    func setUpPeopleResults(at index: Int) {
+        selectedIndexOfCollectionLists = index
+        sizeConverterSwitch.selectedSegmentIndex = 0
+        
+        title = entity?.rawValue.capitalized
+        name.text = peopleCollectionList?.results[index].name.capitalized
+        if peopleCollectionList?.results[index].birthYear == "unknown" {
+            resultsOne.text = peopleCollectionList?.results[index].birthYear.capitalized
+        } else {
+            resultsOne.text = peopleCollectionList?.results[index].birthYear
+        }
+        if let homeworldName = peopleCollectionList?.results[index].getPlanetName(personUrl: peopleCollectionList?.results[index].homeworld ?? "", planets: planetCollectionList) {
+            resultsTwo.text = homeworldName.capitalized
+        }
+        if let height = peopleCollectionList?.results[index].height.toDouble() {
+            switch sizeConverterSwitch.selectedSegmentIndex {
+            case 0:
+                let displayedHeight = height/100
+                resultsThree.text = "\(displayedHeight)m"
+            case 1:
+                let displayHeight = height*3.2808
+                resultsThree.text = "\(displayHeight)ft"
+            default:
+                break
+            }
+            var displayedHeight = height
+            displayedHeight = height/100
+            resultsThree.text = "\(displayedHeight)m"
+        }
+        resultsFour.text = peopleCollectionList?.results[index].eyeColor.capitalized.capitalized
+        if peopleCollectionList?.results[index].hairColor == "n/a" || peopleCollectionList?.results[index].hairColor == "none" {
+            resultsFive.text = "No hair"
+        } else {
+            resultsFive.text = peopleCollectionList?.results[index].hairColor.capitalized
+        }
+        let sortedPeople = peopleCollectionList?.results.sorted { $0.comparableHeight < $1.comparableHeight }
+        guard let totalEntitiesInCollection = sortedPeople?.count else { return }
+        var temporaryIndex = 0
+        while sortedPeople?[temporaryIndex].height == "unknown" {
+            temporaryIndex += 1
+        }
+        smallestEntity.text = sortedPeople?[temporaryIndex].name
+        largestEntity.text = sortedPeople?[totalEntitiesInCollection-1].name
+    }
+    
+    func setUpVehicleResults(at index: Int) {
         selectedIndexOfCollectionLists = index
         sizeConverterSwitch.selectedSegmentIndex = 0
         costConverterSwitch.selectedSegmentIndex = 0
-        switch entity {
-        case .people:
-            title = entity.rawValue.capitalized
-            name.text = peopleCollectionList?.results[index].name.capitalized
-            if peopleCollectionList?.results[index].birthYear == "unknown" {
-                resultsOne.text = peopleCollectionList?.results[index].birthYear.capitalized
-            } else {
-                resultsOne.text = peopleCollectionList?.results[index].birthYear
-            }
-            if let homeworldName = peopleCollectionList?.results[index].getPlanetName(personUrl: peopleCollectionList?.results[index].homeworld ?? "", planets: planetCollectionList) {
-                resultsTwo.text = homeworldName.capitalized
-            }
-            if let height = peopleCollectionList?.results[index].height.toDouble() {
-                switch sizeConverterSwitch.selectedSegmentIndex {
-                case 0:
-                    let displayedHeight = height/100
-                    resultsThree.text = "\(displayedHeight)m"
-                case 1:
-                    let displayHeight = height*3.2808
-                    resultsThree.text = "\(displayHeight)ft"
-                default:
-                    break
-                }
-                var displayedHeight = height
-                displayedHeight = height/100
-                resultsThree.text = "\(displayedHeight)m"
-            }
-            resultsFour.text = peopleCollectionList?.results[index].eyeColor.capitalized.capitalized
-            if peopleCollectionList?.results[index].hairColor == "n/a" || peopleCollectionList?.results[index].hairColor == "none" {
-                resultsFive.text = "No hair"
-            } else {
-                resultsFive.text = peopleCollectionList?.results[index].hairColor.capitalized
-            }
-            let sortedPeople = peopleCollectionList?.results.sorted { $0.comparableHeight < $1.comparableHeight }
-            guard let totalEntitiesInCollection = sortedPeople?.count else { return }
-            var temporaryIndex = 0
-            while sortedPeople?[temporaryIndex].height == "unknown" {
-                temporaryIndex += 1
-            }
-            smallestEntity.text = sortedPeople?[temporaryIndex].name
-            largestEntity.text = sortedPeople?[totalEntitiesInCollection-1].name
-        case .vehicles:
-            title = entity.rawValue.capitalized
-            name.text = vehicleCollectionList?.results[index].name.capitalized
-            resultsOne.text = vehicleCollectionList?.results[index].manufacturer.capitalized
-            resultsTwo.text = vehicleCollectionList?.results[index].costInCredits.capitalized
-            if let length = vehicleCollectionList?.results[index].length.toDouble() {
-                resultsThree.text = "\(length)m"
-            }
-            resultsFour.text = vehicleCollectionList?.results[index].vehicleClass.capitalized
-            resultsFive.text = vehicleCollectionList?.results[index].crew.capitalized
-            let sortedVehicles = vehicleCollectionList?.results.sorted { $0.comparableLength < $1.comparableLength }
-            guard let totalEntitiesInCollection = sortedVehicles?.count else { return }
-            var temporaryIndex = 0
-            while sortedVehicles?[temporaryIndex].length == "unknown" {
-                temporaryIndex += 1
-            }
-            smallestEntity.text = sortedVehicles?[temporaryIndex].name.capitalized
-            largestEntity.text = sortedVehicles?[totalEntitiesInCollection-1].name
-        case .starships:
-            title = entity.rawValue.capitalized
-            name.text = starshipCollectionList?.results[index].name.capitalized
-            resultsOne.text = starshipCollectionList?.results[index].manufacturer.capitalized
-            resultsTwo.text = starshipCollectionList?.results[index].costInCredits.capitalized
-            if let length = starshipCollectionList?.results[index].length.toDouble() {
-                resultsThree.text = "\(length)m"
-            }
-            resultsFour.text = starshipCollectionList?.results[index].starshipClass.capitalized
-            resultsFive.text = starshipCollectionList?.results[index].crew.capitalized
-            var temporaryIndex = 0
-            while temporaryIndex < starshipCollectionList?.results.count ?? 0 {
-                let newLength = self.starshipCollectionList?.results[temporaryIndex].length.replacingOccurrences(of: ",", with: "") ?? ""
-                self.starshipCollectionList?.results[temporaryIndex].length = newLength
-                temporaryIndex += 1
-            }
-            let sortedStarships = starshipCollectionList?.results.sorted { $0.comparableLength < $1.comparableLength }
-            guard let totalEntitiesInCollection = sortedStarships?.count else { return }
-            temporaryIndex = 0
-            while sortedStarships?[temporaryIndex].length == "unknown" {
-                temporaryIndex += 1
-            }
-            smallestEntity.text = sortedStarships?[temporaryIndex].name.capitalized
-            largestEntity.text = sortedStarships?[totalEntitiesInCollection-1].name.capitalized
-        default:
-            break
+        title = entity?.rawValue.capitalized
+        name.text = vehicleCollectionList?.results[index].name.capitalized
+        resultsOne.text = vehicleCollectionList?.results[index].manufacturer.capitalized
+        resultsTwo.text = vehicleCollectionList?.results[index].costInCredits.capitalized
+        if let length = vehicleCollectionList?.results[index].length.toDouble() {
+            resultsThree.text = "\(length)m"
         }
+        resultsFour.text = vehicleCollectionList?.results[index].vehicleClass.capitalized
+        resultsFive.text = vehicleCollectionList?.results[index].crew.capitalized
+        let sortedVehicles = vehicleCollectionList?.results.sorted { $0.comparableLength < $1.comparableLength }
+        guard let totalEntitiesInCollection = sortedVehicles?.count else { return }
+        var temporaryIndex = 0
+        while sortedVehicles?[temporaryIndex].length == "unknown" {
+            temporaryIndex += 1
+        }
+        smallestEntity.text = sortedVehicles?[temporaryIndex].name.capitalized
+        largestEntity.text = sortedVehicles?[totalEntitiesInCollection-1].name
     }
     
+    func setUpStarshipResults(at index: Int) {
+        selectedIndexOfCollectionLists = index
+        sizeConverterSwitch.selectedSegmentIndex = 0
+        costConverterSwitch.selectedSegmentIndex = 0
+        title = entity?.rawValue.capitalized
+        name.text = starshipCollectionList?.results[index].name.capitalized
+        resultsOne.text = starshipCollectionList?.results[index].manufacturer.capitalized
+        resultsTwo.text = starshipCollectionList?.results[index].costInCredits.capitalized
+        if let length = starshipCollectionList?.results[index].length.toDouble() {
+            resultsThree.text = "\(length)m"
+        }
+        resultsFour.text = starshipCollectionList?.results[index].starshipClass.capitalized
+        resultsFive.text = starshipCollectionList?.results[index].crew.capitalized
+        var temporaryIndex = 0
+        while temporaryIndex < starshipCollectionList?.results.count ?? 0 {
+            let newLength = self.starshipCollectionList?.results[temporaryIndex].length.replacingOccurrences(of: ",", with: "") ?? ""
+            self.starshipCollectionList?.results[temporaryIndex].length = newLength
+            temporaryIndex += 1
+        }
+        let sortedStarships = starshipCollectionList?.results.sorted { $0.comparableLength < $1.comparableLength }
+        guard let totalEntitiesInCollection = sortedStarships?.count else { return }
+        temporaryIndex = 0
+        while sortedStarships?[temporaryIndex].length == "unknown" {
+            temporaryIndex += 1
+        }
+        smallestEntity.text = sortedStarships?[temporaryIndex].name.capitalized
+        largestEntity.text = sortedStarships?[totalEntitiesInCollection-1].name.capitalized
+    }
+    
+    // MARK: - Get Entity data via networking code
     func getListOfPeople(page: Int) {
         SearchResultsController.client.getListOfPeople(page: page) { people, error in
             if let people = people {
@@ -292,7 +287,7 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
                     self.getListOfPeople(page: self.entityPageCounter)
                 } else {
                     self.setUpLabelsBasedOnEntity(entity: .people)
-                    self.setUpResultsBasedOnEntity(entity: .people, index: 0)
+                    self.setUpPeopleResults(at: 0)
                     self.pickerView.delegate = self
                     self.pickerView.dataSource = self
                     self.showActivityIndicator(on: false)
@@ -318,7 +313,7 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
                     self.getListOfVehicles(page: self.entityPageCounter)
                 } else {
                     self.setUpLabelsBasedOnEntity(entity: .vehicles)
-                    self.setUpResultsBasedOnEntity(entity: .vehicles, index: 0)
+                    self.setUpVehicleResults(at: 0)
                     self.pickerView.delegate = self
                     self.pickerView.dataSource = self
                     self.showActivityIndicator(on: false)
@@ -344,7 +339,7 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
                     self.getListOfStarships(page: self.entityPageCounter)
                 } else {
                     self.setUpLabelsBasedOnEntity(entity: .starships)
-                    self.setUpResultsBasedOnEntity(entity: .starships, index: 0)
+                    self.setUpStarshipResults(at: 0)
                     self.pickerView.delegate = self
                     self.pickerView.dataSource = self
                     self.showActivityIndicator(on: false)
@@ -380,15 +375,15 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
         let errorMessage: String?
         switch error {
         case SWAPIError.requestFailed:
-            errorMessage = "Your request failed!"
+            errorMessage = "Your request failed!\nPress 'OK' and then click the 'DONE' button\nto go back and try again\nafter checking your Internet connection."
         case SWAPIError.responseUnsuccessful:
-            errorMessage = "No response from server!"
+            errorMessage = "No response from server!\nPlease try again later."
         case SWAPIError.badRequestResponse:
-            errorMessage = "The http response status code was not valid!"
+            errorMessage = "The http response status code was not valid!\nPlease try again later."
         case SWAPIError.jsonParsingFailure:
-            errorMessage = "The JSON could not be Parsed!"
+            errorMessage = "The JSON could not be Parsed!\nPlease contact the App developer\nat abc@123.com."
         case SWAPIError.invalidUrl:
-            errorMessage = "The URL was invalid!"
+            errorMessage = "The URL was invalid!\nWeird... since it's hardcoded. :)"
         default:
             errorMessage = "There was an error!"
         }
@@ -399,6 +394,7 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
         showActivityIndicator(on: false)
     }
     
+    // MARK: -  UIPickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -431,19 +427,11 @@ class SearchResultsController: UITableViewController, UIPickerViewDelegate, UIPi
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if entity?.rawValue == "people" {
-            setUpResultsBasedOnEntity(entity: .people, index: row)
+            setUpPeopleResults(at: row)
         } else if entity?.rawValue == "vehicles" {
-            setUpResultsBasedOnEntity(entity: .vehicles, index: row)
+            setUpVehicleResults(at: row)
         } else {
-            setUpResultsBasedOnEntity(entity: .starships, index: row)
-        }
-    }
-
-    func showActivityIndicator(on: Bool) {
-        if on {
-            activityIndicator.startAnimating()
-        } else {
-            activityIndicator.stopAnimating()
+            setUpStarshipResults(at: row)
         }
     }
 }
